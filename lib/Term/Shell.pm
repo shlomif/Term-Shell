08a562bc6d3f5f78be7f1a5bdee2e7a146407287
Shlomi Fish <shlomif@shlomifish.org>
2016-04-13 10:56:59 +0300

    - Bump version to 0.08

--- lib/Term/Shell.pm
+++ lib/Term/Shell.pm
@@ -10,7 +10,7 @@ use Term::ReadLine;
 
 use vars qw($VERSION);
 
-$VERSION = '0.07';
+$VERSION = '0.08';
 
 #=============================================================================
 # Term::Shell API methods
@@ -915,7 +915,7 @@ package Term::Shell::OnScopeLeave;
 
 use vars qw($VERSION);
 
-$VERSION = '0.07';
+$VERSION = '0.08';
 
 sub new {
     return bless [@_[1 .. $#_]], ref($_[0]) || $_[0];
